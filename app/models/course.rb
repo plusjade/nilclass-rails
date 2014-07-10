600d8e89a1600b0a12a3136a3766ecb4742b57c0
Jade Dominguez <plusjade@gmail.com>
2014-07-10 13:00:22 -0700

    Model courses data in Course class.

--- app/models/course.rb
+++ app/models/course.rb
@@ -18,4 +18,24 @@ class Course
   def readme
     @readme ||= Readme.new( File.open(@content_path).read )
   end
+
+  Dict = {
+    "how-websites-work" => {
+        "name" => "How Websites Work.",
+        "url" => "how-websites-work"
+    },
+
+    "practical-rest" => {
+        "name" => "Practical REST",
+        "url" => "practical-rest"
+    }
+  }
+
+  def self.all
+    Dict.values
+  end
+
+  def self.find(name)
+    Dict[name]
+  end
 end
