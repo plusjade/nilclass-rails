8debbbdb13d7c93e2f8b996a6ef4fc4f573cd175
Jade Dominguez <plusjade@gmail.com>
2014-07-10 12:35:17 -0700

    Fix courses_path endpoints.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Narly::Application.configure do
-  config.git_repo_path = '/Users/jade/dropbox/active/git-tutorials/'
+  config.courses_path = '/Users/jade/dropbox/active/nilclass/public/courses/'
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Narly::Application.configure do
-  config.git_repo_path = '/home/ruhoh/git-tutorials/'
+  config.courses_path = Rails.root.join('public', 'courses')
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
